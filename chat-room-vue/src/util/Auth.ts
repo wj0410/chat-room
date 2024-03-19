bc5c78c05c8a4af7aece8f2c228f712838cd9862
wangjie <89757316@qq.com>
2024-03-19 22:28:58 +0800

    vue

--- chat-room-vue/src/util/Auth.ts
+++ chat-room-vue/src/util/Auth.ts
@@ -1,24 +1,13 @@
-import User from "@/model/User";
-
 const TokenKey = 'token'
-const UserKey = 'userInfo'
 
 export function getToken() {
     return localStorage.getItem(TokenKey)
 }
 
-export function setToken(token) {
+export function setToken(token:string) {
     return localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
     return localStorage.removeItem(TokenKey)
-}
-
-export function getLoginUser(): User | undefined {
-    return new User(<string>localStorage.getItem(UserKey))
-}
-
-export function setLoginUser(json: string) {
-    localStorage.setItem(UserKey, json)
 }
